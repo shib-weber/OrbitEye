c0127f23b2e5e508a6f0dbd0b186a035e7312b1c
Shib <shibjyoti252@gmail.com>
2026-08-20 21:03:10 +0530

    V0.1.6

--- server/main.py
+++ server/main.py
@@ -16,7 +16,7 @@ app = FastAPI(title="OrbitEye API", version="4.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*","https://client-six-alpha-50.vercel.app",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
